0a2412d0a99eac1b8a603b2f2673e53d644fb038
Idan Elfasi <idan60167@gmail.com>
2024-01-30 20:44:56 +0200

    .Limit the ball size diameter to 400px. If the diameter grows beyond 400px, reset it to 100px.

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
  function clickBall(){
     // console.log('You click the Ball')
  }
+ function limitSize(){
+   const elBall= document.querySelector('.ball')
+   if(elBall.style.width>400){
+    elBall.style.width>400
+    elBall.style.hight>400
+   }
+ }
