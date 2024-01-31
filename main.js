819b0de9dcb33f2f94c14993e951235272c9530e
Idan Elfasi <idan60167@gmail.com>
2024-01-31 18:54:51 +0200

    Add a getRandomColor() function to the util.js file and change the ball’s color to a random color each time it is clicked

--- main.js
+++ main.js
@@ -5,13 +5,16 @@
   var  diameter=+elBall.innerText+increment
   console.log(diameter)
   elBall.innerText=diameter
-  elBall.style.width=`${diameter}`;
-  elBall.style.height=`${diameter}`;
-  console.log(elBall.style.width)
+  elBall.style.width=diameter+'px'
+  elBall.style.height=diameter+'px'
+  var randColor=getRandomColor()
+  elBall.style.backgroundColor=randColor
+  
+
  }
 //  function limitSize(){
 //    const elBall= document.querySelector('.ball')
-//    if(elBall.style.width>400){
+//    if(elBall.style.width>400+'px'){
 //     elBall.style.width>400
 //     elBall.style.hight>400
 //    }
