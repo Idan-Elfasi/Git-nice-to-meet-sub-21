0040d7df53df1423ddd4c5d178d435a6d9fc8a71
Idan Elfasi <idan60167@gmail.com>
2024-01-31 18:46:30 +0200

    o increment the ball’s diameter by a random amount between 20px and 60px.

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
- function clickBall(){
-    // console.log('You click the Ball')
+
+ function onBallClick(){
+  const elBall= document.querySelector('.ball')
+  var increment=getRandomInt(20,61)
+  var  diameter=+elBall.innerText+increment
+  console.log(diameter)
+  elBall.innerText=diameter
+  elBall.style.width=`${diameter}`;
+  elBall.style.height=`${diameter}`;
+  console.log(elBall.style.width)
  }
- function limitSize(){
-   const elBall= document.querySelector('.ball')
-   if(elBall.style.width>400){
-    elBall.style.width>400
-    elBall.style.hight>400
-   }
- }
+//  function limitSize(){
+//    const elBall= document.querySelector('.ball')
+//    if(elBall.style.width>400){
+//     elBall.style.width>400
+//     elBall.style.hight>400
+//    }
+//  }
