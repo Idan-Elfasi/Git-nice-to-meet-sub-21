bc69890726dab6cff306144dd548d3e19565c285
Idan Elfasi <idan60167@gmail.com>
2024-01-31 19:29:51 +0200

    Add a maxDiameter parameter to onBallClick() which will be used as an upper limit to the ball’s size. If a ball reaches its diameter limit, its size is reset to 100px. In the HTML file, pass different values as arguments to the function calls of each ball

--- main.js
+++ main.js
@@ -1,16 +1,41 @@
 
- function onBallClick(){
+ function onBallClick(max){
   const elBall= document.querySelector('.ball')
   var increment=getRandomInt(20,61)
   var  diameter=+elBall.innerText+increment
   console.log(diameter)
   elBall.innerText=diameter
-  elBall.style.width=diameter+'px'
-  elBall.style.height=diameter+'px'
+  if(diameter>max){
+    elBall.style.width=100+'px'
+    elBall.style.height=100+'px'
+    elBall.innerText=100
+  }
+  else{
+    elBall.innerText=diameter
+    elBall.style.width=diameter+'px'
+    elBall.style.height=diameter+'px'
+  }
   var randColor=getRandomColor()
   elBall.style.backgroundColor=randColor
-  
+ }
+ function onBallClick2(max){
+  const elBall= document.querySelector('.ball2')
+  var increment=getRandomInt(20,61)
+  var  diameter=+elBall.innerText+increment
+  console.log(diameter)
+  if(diameter>max){
+    elBall.style.width=100+'px'
+    elBall.style.height=100+'px'
+    elBall.innerText=100
 
+  }
+  else{
+    elBall.innerText=diameter
+    elBall.style.width=diameter+'px'
+    elBall.style.height=diameter+'px'
+  }
+  var randColor=getRandomColor()
+  elBall.style.backgroundColor=randColor
  }
 //  function limitSize(){
 //    const elBall= document.querySelector('.ball')
